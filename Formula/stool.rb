8c4f1814728871f3903230a0de66bd03a84a4fd3
haijima <k.haijima@gmail.com>
2023-12-04 21:47:36 +0900

    Fix description to starts with verb

--- Formula/stool.rb
+++ Formula/stool.rb
@@ -1,5 +1,5 @@
 class Stool < Formula
-  desc "An access log profiler"
+  desc "Profiles access log"
   homepage "https://github.com/haijima/stool"
   url "https://github.com/haijima/stool/releases/download/v1.16.0/stool_Darwin_x86_64.tar.gz"
   sha256 "06ec8e156f6b961e50def8c09cc19be8eb5c01b2213bfedf40ebb14fc08b3f7e"
