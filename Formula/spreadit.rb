44275a857bc8143e9c59459696134c952778d446
haijima <k.haijima@gmail.com>
2023-12-04 21:40:30 +0900

    Fix description to start with a capital letter

--- Formula/spreadit.rb
+++ Formula/spreadit.rb
@@ -1,5 +1,5 @@
 class Spreadit < Formula
-  desc "spreadit is a simple CLI tool to write CSV data to Google Sheets"
+  desc "Spreadit is a simple CLI tool to write CSV data to Google Sheets"
   homepage "https://github.com/haijima/spreadit"
   url "https://github.com/haijima/spreadit/releases/download/v1.2.1/spreadit_Darwin_x86_64.tar.gz"
   sha256 "37a41a46ec3868d549ebeaaa04b73b82d52e9183be29a1c227fa4fc0ace3f0b2"
--- Formula/stool.rb
+++ Formula/stool.rb
@@ -1,5 +1,5 @@
 class Stool < Formula
-  desc "stool is access log profiler"
+  desc "Stool is access log profiler"
   homepage "https://github.com/haijima/stool"
   url "https://github.com/haijima/stool/releases/download/v1.16.0/stool_Darwin_x86_64.tar.gz"
   sha256 "06ec8e156f6b961e50def8c09cc19be8eb5c01b2213bfedf40ebb14fc08b3f7e"
