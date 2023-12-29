cdeaa23ebcc9f9a086e3c93158ddf2d71c41275c
haijima <k.haijima@gmail.com>
2023-12-29 17:39:00 +0900

    Update stool version

--- Formula/stool.rb
+++ Formula/stool.rb
@@ -1,8 +1,8 @@
 class Stool < Formula
   desc "Profiles access log"
   homepage "https://github.com/haijima/stool"
-  url "https://github.com/haijima/stool/releases/download/v1.16.0/stool_Darwin_x86_64.tar.gz"
-  sha256 "06ec8e156f6b961e50def8c09cc19be8eb5c01b2213bfedf40ebb14fc08b3f7e"
+  url "https://github.com/haijima/stool/releases/download/v1.19.0/stool_Darwin_x86_64.tar.gz"
+  sha256 "b7ddec2a822820a6722625fd49b63b903ff539a55975240dfa1f132e9b3a9be8"
   license "MIT"
 
   def install
